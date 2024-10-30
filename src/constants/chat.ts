436cfbd8e4524daccbea091c611c6538bee4ae9b
*NULL <58626061+N48LL@users.noreply.github.com>
2024-10-30 21:33:53 +0100

    Update chat.ts

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -58,6 +58,7 @@ export const modelMaxToken = {
   'gpt-4-turbo-2024-04-09': 128000,
   'gpt-4o': 128000,
   'gpt-4o-2024-05-13': 128000,
+  'o1-preview': 128000,
 };
 
 export const modelCost = {
@@ -134,6 +135,10 @@ export const modelCost = {
     completion: { price: 0.015, unit: 1000 },
   },
   'gpt-4o-2024-05-13': {
+    prompt: { price: 0.005, unit: 1000 },
+    completion: { price: 0.015, unit: 1000 },
+  },
+    'o1-preview': {
     prompt: { price: 0.005, unit: 1000 },
     completion: { price: 0.015, unit: 1000 },
   },
